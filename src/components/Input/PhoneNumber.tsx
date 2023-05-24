001e483fc7e48fc05ce55409a40ad42c22674c69
Abimbola Oladele <bimbooladele@outlook.com>
2023-05-24 13:21:48 +0100

    fix phone input

--- src/components/Input/PhoneNumber.tsx
+++ src/components/Input/PhoneNumber.tsx
@@ -37,7 +37,6 @@ const PhoneNumberInput: FC<Props> = ({
           paddingTop: "1.25rem",
           paddingBottom: "1.25rem",
           height: "100%",
-          borderRadius: "40px",
         }}
         value={value}
         containerClass="items-center mr-5"
