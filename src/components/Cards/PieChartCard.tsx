ec1bdb2443d050ba80b2bd2117e8749f6b96ba53
chijooke <michaelsylva36@gmail.com>
2023-06-26 14:28:49 +0100

    chore(feat): customer dashboard data

--- src/components/Cards/PieChartCard.tsx
+++ src/components/Cards/PieChartCard.tsx
@@ -1,15 +1,25 @@
+import { DashboardDataType } from "@/pages";
 import { FC } from "react";
 import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
 
-const data = [
-  { name: "Surulere", value: 100, fill: "#b6b6bd" },
-  { name: "Badagdry", value: 150, fill: "#ff9202" },
-];
-const total = data.reduce((a, b) => a + b.value, 0);
 type Props = {
   title: string;
+  data:
+    | {
+        storageSpace: string;
+        storageLocation: string;
+      }[]
+    | undefined;
 };
-const PieChartCard: FC<Props> = ({ title }) => {
+const PieChartCard: FC<Props> = ({ title, data }) => {
+  const pieChartData =
+    data?.map((item, index) => ({
+      name: "Storage Loca",
+      value: item?.storageLocation,
+      fill: index % 2 === 0 ? "#b6b6bd" : "#ff9202",
+    })) || [];
+
+  const total = pieChartData.reduce((a, b) => a + Number(b.value), 0);
   return (
     <div className=" p-4 gap-10 rounded border-2  border-gray-300 items-center  w-full">
       <p className="font-extrabold text-[14px]">{title}</p>
